8f9c435478986ee509a4ed4aa9080bc768ca5553
Kinskey <kinskeyi@onesource.com>
2017-02-27 17:06:06 -0600

    Some more stuff

--- analysis/bronx_homes_analysis.R
+++ analysis/bronx_homes_analysis.R
@@ -1,3 +1,5 @@
 bx.clean <- read.csv("data/bronx_rollingsales_homes_clean.csv",header=TRUE)
 summary(bx.clean)
 plot(log10(bx.clean$gross.sqft),log10(bx.clean$sale.price.n))
+bx.clean$price.per.sqft<- bx.clean$sale.price.n / bx.clean$gross.sqft
+
